470798958d9ac940c1ba763ee7ad0d1436909995
michel-hategekimana <michelhategekimana@127gmail.com>
2026-01-19 19:48:53 +0200

    verify if a user have access

---
+++ src/midleware/verifyAccess.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+export function verifyAccess(passRole){
+    return (req,res,next)=>{
+        const token =req.headers['auth-token']
+        console.log(token)
+    if(!token){
+        return res.status(404).json({message:"no token provided"})
+    }else{
+        try {
+            const verifyToken= jwt.verify(token,process.env.SECRET_KEY,{expiresIn:"1d"})
+        req.body=verifyToken.user
+        if(passRole != verifyToken.user.role){
+            return res.status(401).json({message:"Please you don't have access"})
+        }else{
+            return next()
+        }
+        } catch (error) {
+         if((error.name="JsonWebTokenError")){
+            return res.status(401).json({message:"Invalid token or expired token"})
+         }else{
+            return res.status(500).json({message:`Error is ${error}`})
+         }
+            
+        }
+    }
+
+    }
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import Controller from "../controller/userController.js"
 import { EmailExist } from "../midleware/validation.js"
+import { verifyAccess } from "../midleware/verifyAccess.js"
 
 const router=express.Router()
 router.post("/user",EmailExist,Controller.signup)
 router.post("/user/login",Controller.login)
 router.delete("/user/:id",Controller.deleteOneUser)
-router.get("/users",Controller.getAllUsers)
+router.get("/users",verifyAccess("provider"),Controller.getAllUsers)
 router.get("/user/:id",Controller.getOneUser)
 router.patch("/user/:id",Controller.updateOneUser)
 router.delete("/users",Controller.deleteAllUsers)
